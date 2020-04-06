aa7fe0eb24a21e1318fb359084a2ffeabf767aeb
Sachin Bansal <sachin.bansal@nagarro.com>
2020-04-06 22:07:13 +0530

    Initial Push

---
+++ flask-sample/app/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import socket    
+
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+  hostname = socket.gethostname()    
+  IPAddr = socket.gethostbyname(hostname)    
+  return "Your Computer Name is: " + hostname + "\nYour Computer IP Address is: "+ IPAddr
+  
+if __name__ == "__main__":
+  # app.run(debug=True, host='127.0.0.1')
+  app.run(debug=True, host='0.0.0.0')
